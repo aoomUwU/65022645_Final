78d3657d207e4b649b476c640b7f506cf89678bd
aoomUwU <65022645@up.ac.th>
2023-02-22 14:53:18 +0700

    01
    01

--- fJAVAS.js
+++ fJAVAS.js
@@ -1,1 +1,25 @@
-console.log(document.querySelector('#a'));
+function submitForm() {
+    var name = document.getElementById("name").value
+    var surname = document.getElementById("surname").value
+    var birth = document.getElementById("birth").value
+    var grender = document.getElementById("grender").value
+    var result = document.getElementById("result")
+    if (grender && surname && birth && name) {
+        let R = document.createElement("tr")
+        result.appendChild(R)
+        let creaName = document.createElement("td")
+        let creaSurname = document.createElement("td")
+        let creaGrender = document.createElement("td")
+        let Birth_Date = document.createElement("td")
+
+        creaName.innerText = name
+        creaSurname.innerText = surname
+        creaGrender.innerText = grender
+        Birth_Date.innerText = birth
+
+        R.appendChild(creaName)
+        R.appendChild(creaSurname)
+        R.appendChild(creaGrender)
+        R.appendChild(Birth_Date)
+    }
+}
